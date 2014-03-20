c245facd00bc446970570cbee0e85d32febd6446
SONY <SONY@Meera>
2014-03-20 16:40:23 +0530

    Thanks for commit

---
+++ Car/src/Hyundai.java
@@ -0,0 +1,12 @@
+
+public class Hyundai {
+	String ReverseSensor,Geartype;
+	public void setGearType(String Geartype)
+	{
+		this.Geartype=Geartype;
+	}
+	public void setReverseSensor(String ReverseSensor)
+	{
+		this.ReverseSensor=ReverseSensor;
+	}
+}
---
+++ Car/src/Maruti.java
@@ -0,0 +1,13 @@
+
+public class Maruti {
+	String SteeringType,WindowType;
+	public void setSteeringType(String SteeringType)
+	{
+		this.SteeringType=SteeringType;
+	}
+	public void setWindowType(String WindowType)
+	{
+		this.WindowType=WindowType;
+	}
+	
+}
